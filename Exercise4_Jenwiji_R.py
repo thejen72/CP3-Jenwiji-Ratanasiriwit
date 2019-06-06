8c381d21e16f5485264ae0946b53ebfdd61367f0
thejen72 <jzsn.jzsn@gmail.com>
2019-06-06 10:17:38 +0700

    Created Variables & Display

---
+++ Exercise4_Jenwiji_R.py
@@ -0,0 +1,9 @@
+FE = 46
+GB = 65
+ICS = 87
+CP = 91
+print("-----Your Score-----")
+print("Foundation English:                  ", FE)
+print("General Business:                    ", GB)
+print("Introduction to Computer Systems:    ", ICS)
+print("Computer Programming:                ", CP)
