76e3f2f631fce640e6dfdfbf1742e29267f98503
thejen72 <jzsn.jzsn@gmail.com>
2019-06-09 14:38:08 +0700

    Created Functions

---
+++ Lecture50_Jenwiji_R.py
@@ -0,0 +1,15 @@
+def addNumber(x, y, z):
+    print(x+y+z)
+addNumber(10, 4, 8)
+
+def subtractNumber(a, b):
+    print(a-b)
+subtractNumber(10, 3)
+
+def multiplyNumber(c, d):
+    print(c*d)
+multiplyNumber(9, 5)
+
+def divideNumber(m, n):
+    print(m/n)
+divideNumber(20, 5)
