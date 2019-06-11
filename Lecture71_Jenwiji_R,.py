c638b7296b7e7290a6babbdb234a8fd11246563c
thejen72 <jzsn.jzsn@gmail.com>
2019-06-11 16:45:39 +0700

    Supermarket with Lists

---
+++ Lecture71_Jenwiji_R,.py
@@ -0,0 +1,21 @@
+itemList = []
+priceList = []
+
+def myReceipt():
+    total =0
+    for myItems in range(len(itemList)):
+        print(itemList[myItems], priceList[myItems])
+        total += int(priceList[myItems])
+    print("Total amount:    ", total)
+
+print("welcome to my supermarket")
+while True:
+    item = input("Please input item:     ")
+    if(item.lower()=="exit"):
+        break
+    else:
+        price = input("Please input price:  ")
+        itemList.append(item)
+        priceList.append(price)
+
+myReceipt()
